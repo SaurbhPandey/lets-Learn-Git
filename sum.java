99710fbc83620941932e5787c553e7e164cc324e
Saurabh Pandey <spandey882000@gmail.com>
2022-07-02 23:03:04 +0530

    Sum.java file commited for the first time

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 28;
+        int sum = a+b;
+        System.out.println("Sum of two numbers will be " + sum);
+    }
+}
