2dd24c6f8c5cd05ada782a9fb8e32963644b9675
Saurabh Pandey <spandey882000@gmail.com>
2022-07-03 00:03:51 +0530

    Diff.java is comitted for the third time

--- Diff.java
+++ Diff.java
@@ -1,5 +1,8 @@
 public class Diff{
     public static void main(String[] args) {
-        
+        int a = 8;
+        int b = 5;
+        int diff = a-b;
+        System.out.println("The Diff of two numbers will be " + diff);
     }
 }
