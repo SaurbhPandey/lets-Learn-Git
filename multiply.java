3a96a0c480ece04f80253dff4520a03f67901464
Saurabh Pandey <spandey882000@gmail.com>
2022-07-02 23:18:06 +0530

    multiply.java commited for the first time

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply{
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 8;
+        int res = a*b;
+        System.out.println("Product of two numbers will be " + res);
+    }
+}
