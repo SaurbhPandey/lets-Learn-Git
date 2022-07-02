2795751c37b3076adea20911b8773bc4931ad756
Saurabh Pandey <spandey882000@gmail.com>
2022-07-03 00:19:10 +0530

    division.java is commited for the first time

---
+++ division.java
@@ -0,0 +1,8 @@
+public class division{
+    public static void main(String[] args) {
+        int a = 18;
+        int b = 2;
+        int res = a/b;
+        System.out.println("The division of these numbers will be "+res);
+    }
+}
