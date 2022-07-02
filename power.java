5935a843570b383e020d1de02509510cc7e72787
Saurabh Pandey <spandey882000@gmail.com>
2022-07-02 23:32:01 +0530

    power.java is comitted for the first time

---
+++ power.java
@@ -0,0 +1,7 @@
+public class power{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println("Power of a^b is "  + Math.pow(a, b));
+    }
+}
